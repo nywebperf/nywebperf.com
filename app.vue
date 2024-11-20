47ebadcfb6ea6c0a7b3ae5f7eebf3399989a1471
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2024-11-20 13:54:31 -0500

    set up pages, filled out initial main page

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+  <main>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+  </main>
 </template>
---
+++ pages/main/index.vue
@@ -0,0 +1,26 @@
+<script>
+// Timer logic will be here
+</script>
+
+<template>
+    <section>
+        <h1>URL Shortener</h1>
+        <p>
+            We will have a timer
+        </p>
+    </section>
+</template>
+
+<style>
+body {
+    background-color: rgb(37, 36, 34);
+    color: rgb(236, 225, 196);
+}
+
+section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 5rem;
+}
+</style>
