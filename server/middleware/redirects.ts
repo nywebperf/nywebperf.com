01b77204cf4a4b7ec0c134596ca0598d007ab2f0
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2024-11-24 17:42:37 -0500

    we have a basic url shortener

--- server/middleware/redirects.ts
+++ server/middleware/redirects.ts
@@ -1,3 +1,25 @@
+
+const redirects = [
+    {
+        destinationURL: 'https://alexchernyshev.com/',
+        shortURL: 'alex',
+        releaseDate: new Date('July 18, 2024 12:00:00')
+    },
+    {
+        destinationURL: 'https://www.sergeychernyshev.com/',
+        shortURL: 'sergey',
+        releaseDate: new Date('July 16, 2024 12:00:00')
+    },
+];
+
 export default defineEventHandler(async (event) => {
+
+    const { path } = event;
+
+    for (const redirect of redirects) {
+        if ("/" + redirect.shortURL === path) {
+            return sendRedirect(event, redirect.destinationURL, 307);
+        }
+    }
 
 });
