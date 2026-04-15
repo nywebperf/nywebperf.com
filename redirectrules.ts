a3510b86bc435711da6d0a0ea85e1f6c5436257e
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2026-04-15 13:13:58 -0400

    All links from nywebperformance.com

--- redirectrules.ts
+++ redirectrules.ts
@@ -9,6 +9,50 @@ const redirects = [
     {
         destinationURL: 'https://docs.google.com/forms/d/e/1FAIpQLSfHyWHSWntTLEu_6pq500PhrrAjyyyVZsH92_eFK-SLMUJD5Q/viewform',
         shortURL: 'speak',
+    },
+    {
+        destinationURL: 'https://www.linkedin.com/company/web-performance-ny/',
+        shortURL: 'li',
+    },
+    {
+        destinationURL: 'https://www.linkedin.com/company/web-performance-ny/',
+        shortURL: 'linkedin',
+    },
+    {
+        destinationURL: 'https://bsky.app/profile/nywebperf.com',
+        shortURL: 'bsky',
+    },
+    {
+        destinationURL: 'https://bsky.app/profile/nywebperf.com',
+        shortURL: 'bluesky',
+    },
+    {
+        destinationURL: 'https://www.facebook.com/nywebperf/',
+        shortURL: 'fb',
+    },
+    {
+        destinationURL: 'https://www.facebook.com/nywebperf/',
+        shortURL: 'facebook',
+    },
+    {
+        destinationURL: 'https://docs.google.com/forms/d/e/1FAIpQLSe8aB-He39hMq5ywVxVdFPJCnXU3-a6ifyYSdLp-3aJ7y_eiA/viewform',
+        shortURL: 'meet4speed',
+    },
+    {
+        destinationURL: 'https://docs.google.com/forms/d/e/1FAIpQLSe8aB-He39hMq5ywVxVdFPJCnXU3-a6ifyYSdLp-3aJ7y_eiA/viewform',
+        shortURL: 'm4s',
+    },
+    {
+        destinationURL: 'https://www.nywebperformance.org/',
+        shortURL: 'org',
+    },
+    {
+        destinationURL: 'https://www.nywebperformance.org/',
+        shortURL: 'organize',
+    },
+    {
+        destinationURL: 'https://www.nywebperformance.org/',
+        shortURL: 'organizers',
     }
 ];
 
